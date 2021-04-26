086c5fd8de40664cd82a5121585f3eb2dbc72309
imn1oy <jaro1221@gmail.com>
2021-04-26 22:07:32 +0200

    migrate to Java

---
+++ README.md
@@ -0,0 +1,33 @@
+# Sztuczna Inteligencja - Projekt Optymalizacyjny
+
+Firma "AI.Tech Solutions", dostarczająca rozwiązania IT, prowadzi swój sklep internetowy.
+Z uwagi na coraz większe zainteresowanie marką, CEO decyduje się na utworzenie sieci magazynów rozproszonych po kilku miastach w całej Polsce.
+Firma chce powiększych swój asortyment, dlatego postanawia przenieść znaczną część produktów z magazynu głownego do kilku mniejszych.
+W tym celu zostaje wynajęta firma transportowa dysponująca określoną flotą.
+
+------------
+
+
+##### Problem: w jaki sposób załadować samochody transportowe aby koszt całego przedsięwzięcia był jak najniższy
+
+------------
+
+
+Kluczowe założenia:
+1) Siedziba głownego magazynu znajduje się w Szczecinie. Miasta w których mają powstać magazyny: Olsztyn, Siedlce, Kielce, Opole, Zielona Góra i Bydgoszcz.
+2) Produkty zostają zapakowane do kartonów. Każdy z nich ma określone wymiary oraz masę, która powinna zostać uwzględniona podczas załadunku.
+3) Każdy z pojazdów transportowych ma określone wymiary przestrzeni załadunkowej oraz ładowność.
+4) Dostępna flota pojazdów (jak również przestrzeń bagażowa, ładowność oraz cena za 1km):
+
+  - Citroen Berlingo II Van   | 180x123x125       | 727 kg    | 1,05zł
+  - Renault Trafic II Furgon  | 240x169x128.5     | 1630 kg   | 1,20zł
+  - Peugeot Boxer II Furgon   | 267x156.2x166.2   | 1850 kg   | 1,25zł
+  - Fiat Ducato III Maxi      | 370.5x187x193.2   | 2340 kg   | 1,65zł
+  - Fiat Ducato III           | 312x165x193.2     | 1996 kg   | 1,45zł
+  - Opel Movano III Furgon    | 217.5x157.7x189.4 | 1487 kg   | 1,25zł
+
+------------
+
+
+###### Realizacja projektu: język programowania JavaScript/Node.js, IDE: Visual Studio Code
+###### Jarosław Przyjemski, 19254, III SN
---
+++ src/main/java/com/si/Main.java
@@ -0,0 +1,88 @@
+package com.si;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    private static final int HS = 10;
+    private static final double maxCarMass = 2340.0;      // kg
+    private static final double maxCarCube = 13385572.2;    // cm^3
+    public static JSONArray products;
+    public static ArrayList<ArrayList<String>> HM;
+    public static ArrayList<ArrayList<Double>> FP;
+
+    public static void main(String[] args) {
+        HM = new ArrayList<>();
+        FP = new ArrayList<>();
+        loadFiles();
+
+        // MAIN LOOP
+        for (int i = 0; i < HS; i++) {
+            ArrayList<String> vectHM = new ArrayList<>();
+            double currCube = 0.0;
+            double currMass = 0.0;
+
+            //get random product
+            while (true) {
+                int rnd = new Random().nextInt(products.size());
+                JSONObject prod = (JSONObject) products.get(rnd);
+                String prodId = (String) prod.get("id");
+
+                // avoid duplicates
+                if (!vectHM.contains(prodId)) {
+                    // check cube
+                    double prodCube = (double) prod.get("dl") * (double) prod.get("szer") * (double) prod.get("wys");
+                    if ((currCube + prodCube) > maxCarCube) break;
+                    else currCube = currCube + prodCube;
+
+                    //check mass
+                    double prodMass = (double) prod.get("masa");
+                    if ((currMass + prodMass) > maxCarMass) break;
+                    else currMass = currMass + prodMass;
+
+                    // ok, add to solution vector
+                    vectHM.add(prodId);
+                }
+
+            }
+
+            HM.add(vectHM);
+            ArrayList<Double> vectFP = new ArrayList<>();
+            vectFP.add(currCube);
+            vectFP.add(currMass);
+            FP.add(vectFP);
+
+        }
+        printMatrix();
+    }
+
+    private static void printMatrix() {
+        System.out.println("HM = ");
+        for (int i = 0; i < HS; i++) {
+            System.out.println(HM.get(i));
+        }
+
+        System.out.println("\nFP = ");
+        for (int i = 0; i < HS; i++) {
+            System.out.println(FP.get(i));
+        }
+    }
+
+    public static void loadFiles() {
+        JSONParser jsonParser = new JSONParser();
+        try {
+            Object obj = jsonParser.parse(new FileReader("files/produkty.json"));
+            //System.out.println(obj);
+            JSONObject jsonObj = (JSONObject) obj;
+            products = (JSONArray) jsonObj.get("productsList");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
